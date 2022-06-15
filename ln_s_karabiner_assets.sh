1b811ffe684ed4dfe0b66311afba7bb43fbde58b
UseK <use.k.james@gmail.com>
2022-06-16 06:04:02 +0900

    Enable Karabiner Emacs keybind for VSCode

---
+++ ln_s_karabiner_assets.sh
@@ -0,0 +1,4 @@
+cd ~/.config/karabiner/assets/complex_modifications
+ln -s ~/dotfiles/command_etc_to_focus_to_menu_bar.json ./
+ln -s ~/dotfiles/command_space_to_kana.json ./
+ln -s ~/dotfiles/my_emacs.json ./
