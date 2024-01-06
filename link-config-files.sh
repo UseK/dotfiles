96c34632cc1de6661d482aa3046cbcd6ed2b6e65
UseK <use.k.james@gmail.com>
2024-01-06 19:42:30 +0900

    Just add config file for starship and link script

--- link-config-files.sh
+++ link-config-files.sh
@@ -1,2 +1,3 @@
 mv ~/.config/git/ignore ~/.config/git/ignore.back.`date +%Y%m%d`
 ln -s ~/dotfiles/dot-config-git-ignore ~/.config/git/ignore
+ln -s ~/dotfiles/dot-config-starship.toml ~/.config/starship.toml
