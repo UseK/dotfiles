932c9655bec17772f9fe5e06aa050a1e81d31df6
UseK <use.k.james@gmail.com>
2024-01-06 20:08:51 +0900

    Add file for hammerspoon(alternative for shiftit) and link script

--- link-config-files.sh
+++ link-config-files.sh
@@ -1,3 +1,5 @@
-mv ~/.config/git/ignore ~/.config/git/ignore.back.`date +%Y%m%d`
+# mv ~/.config/git/ignore ~/.config/git/ignore.back.`date +%Y%m%d`
 ln -s ~/dotfiles/dot-config-git-ignore ~/.config/git/ignore
 ln -s ~/dotfiles/dot-config-starship.toml ~/.config/starship.toml
+mkdir -p ~/.hammerspoon
+ln -s ~/dotfiles/.hammerspoon/init.lua ~/.hammerspoon/init.lua
