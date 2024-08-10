5102cbe071baa9c62989f524aa52b4218ac2b5d3
UseK <use.k.james@gmail.com>
2024-08-10 13:58:20 +0900

    Use Rectangle instead of Hammerspoon

--- link-config-files.sh
+++ link-config-files.sh
@@ -1,5 +1,3 @@
 # mv ~/.config/git/ignore ~/.config/git/ignore.back.`date +%Y%m%d`
 ln -s ~/dotfiles/dot-config-git-ignore ~/.config/git/ignore
 ln -s ~/dotfiles/dot-config-starship.toml ~/.config/starship.toml
-mkdir -p ~/.hammerspoon
-ln -s ~/dotfiles/.hammerspoon/init.lua ~/.hammerspoon/init.lua
