6b8dc0eefe6d90f748427e6ee202946b7d42a95d
UseK <use.k.james@gmail.com>
2024-11-02 15:34:44 +0900

    Enable Rust in fish

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+  fish_add_path $HOME/.cargo/bin
   eval "$(/opt/homebrew/bin/brew shellenv)"
   # Commands to run in interactive sessions can go here
   starship init fish | source
