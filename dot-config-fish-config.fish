a05e56f192ddf5cc4bb66941b693a5eb5d879854
UseK <use.k.james@gmail.com>
2025-08-12 18:02:27 +0900

    Bugfix: cannot use commands installed with pip

--- dot-config-fish-config.fish
+++ dot-config-fish-config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
   fish_add_path $HOME/.cargo/bin
+  # Use commands installed with pip
+  fish_add_path $HOME/Library/Python/3.9/bin
   eval "$(/opt/homebrew/bin/brew shellenv)"
   # https://developer.1password.com/docs/cli/reference/#shell-completion
   if type op &>/dev/null;
