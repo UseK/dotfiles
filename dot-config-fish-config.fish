ce0b0d2fcb102cbe24f02626e7e34d5eae56cd6b
UseK <use.k.james@gmail.com>
2026-01-10 13:29:06 +0900

    Add path for LM Studio CLI

--- dot-config-fish-config.fish
+++ dot-config-fish-config.fish
@@ -48,3 +48,8 @@ end
 # https://docs.volta.sh/guide/getting-started
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH $HOME/.lmstudio/bin
+# End of LM Studio CLI section
+
